5a181809e62b712ab4e8ff940a28c6c71382093e
alexeytagun <alexey.tagun@game-garden.com>
2020-10-03 16:30:29 +0300

    add rotating component for tube

---
+++ Assets/Scripts/Rotaror.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotaror : MonoBehaviour {
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Vector3 _eulerAngleVelocity = new Vector3(0, 0, 100);
+
+    void FixedUpdate()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(_eulerAngleVelocity * Time.deltaTime);
+        _rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
+    }
+}
