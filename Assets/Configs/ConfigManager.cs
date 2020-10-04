5d64d04d9ca72be049084f09ff423ff55bb06c52
alexeytagun <alexey.tagun@game-garden.com>
2020-10-04 17:15:45 +0300

    add config manager

---
+++ Assets/Configs/ConfigManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigManager : MonoBehaviour {
+    [SerializeField] private Config _scriptableObject;
+    public static Config Data;
+
+    private void Awake() {
+        Data = _scriptableObject;
+    }
+}
