917e7f5dd355b2bbf991f05f82170932601754c0
AlexKutsar <alekcandr_123qw@mail.ru>
2020-10-04 03:33:16 +0300

    add button action click:menu and interactiva panels add dotween

--- Assets/Scripts/BtnStart.cs
+++ Assets/Scripts/BtnStart.cs
@@ -5,15 +5,18 @@ using UnityEngine.UI;
 
 public class BtnStart : MonoBehaviour
 {
+    private UIController UIController = null;
     private Button buttonStart = null;
     private SpawnController spawnController = null;
     private void Start()
     {
         spawnController = (SpawnController)FindObjectOfType(typeof(SpawnController));
+        UIController = (UIController)FindObjectOfType(typeof(UIController));
         buttonStart = gameObject.GetComponent<Button>();
         buttonStart.onClick.AddListener(() => {
-            Debug.Log("Click");
             spawnController.SpawnObstacle();
+            UIController.StartCoroutine(UIController.ShowIterationPanel());
+            UIController.MenuPanel.SetActive(false);
         });
     }
 
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public CanvasGroup IterationPanel;
+    public float TimeShowIterationPanel;
+    public float TimeShowAndHideInteractionPanen = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IterationPanel.DOFade(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator ShowIterationPanel()
+    {
+        IterationPanel.DOFade(1, TimeShowAndHideInteractionPanen);
+        yield return new WaitForSeconds(TimeShowIterationPanel + TimeShowAndHideInteractionPanen);
+        IterationPanel.DOFade(0, TimeShowAndHideInteractionPanen);
+    }
+}
